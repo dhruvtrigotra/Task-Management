08418470f8a9e8fded99f75a84a7b66d1a0f3314
rohini <rohinitrigotra17@gmail.com>
2026-05-04 13:23:53 +0530

    fix db config for railway

--- frontend/task-manager-ui/src/api/axios.js
+++ frontend/task-manager-ui/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://task-management-production-7015.up.railway.app",
 });
 
 // attach JWT automatically
